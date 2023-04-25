f86dc2905b19e0ce5bb0ee6417fda7ab98ad2c87
Malgorzata Barej <barejmalgosia@gmail,com>
2023-04-25 09:59:03 +0200

    Zmiany w funkcji dms

--- skrypt.py
+++ skrypt.py
@@ -25,17 +25,11 @@ class Transformacje:
         self.e2 = sqrt(2 * self.flat - self.flat ** 2)
         self.ep2 = (2 * self.flat - self.flat ** 2)
         
-    """Konwersja stopni na stopnie, minuty, sekundy"""
-    def dms(x):
-        degrees=int(x)
-        minutes=int((x-degrees)*60)
-        seconds=((x-degrees)*60-minutes)*60
-        return(degrees, minutes, seconds)
         
     """
     Tranformacja współrzędnych geocentrycznych XYZ na współrzędne elipsoidalne fi, lambda, h
     """
-    def XYZ2flh(self, X, Y, Z, output = "dms"):
+    def XYZ2flh(self, X, Y, Z, output = "dec_degree"):
         """Zastosowano algorytm Hirvonena, transformujący współrzędne prostokątne na współrzędne elipsoidalne. W procesie iteracyjnym, uzyskujemy dokładne wyniki"""
         r   = sqrt(X**2 + Y**2)           # promień
         lat_prev = atan(Z / (r * (1 - self.ep2)))    # pierwsze przybliilizenie
@@ -51,8 +45,8 @@ class Transformacje:
         if output == "dec_degree":
             return degrees(lat), degrees(lon), h 
         elif output == "dms":
-            lat = self.dms(degrees(lat))
-            lon = self.dms(degrees(lon))
+            lat = self.deg2dms(degrees(lat))
+            lon = self.deg2dms(degrees(lon))
             return f"{lat[0]:02d}:{lat[1]:02d}:{lat[2]:.2f}", f"{lon[0]:02d}:{lon[1]:02d}:{lon[2]:.2f}", f"{h:.3f}"
         else:
             raise NotImplementedError ("nieobsługiwana elipsoida")
