beb6289a4e703acc8ce08e5eaf8dc51fa5980970
Malgorzata Barej <barejmalgosia@gmail,com>
2023-04-27 23:54:37 +0200

    wiekszosc dziala jest git

--- skrypt.py
+++ skrypt.py
@@ -11,7 +11,6 @@ from argparse import ArgumentParser
 
 class Transformacje:
     def __init__(self, elip):
-        print('class', elip)
         self.a = elip[0]
         self.ep2 = elip[1]
     
@@ -26,34 +25,29 @@ class Transformacje:
     """
     Tranformacja współrzędnych geocentrycznych XYZ na współrzędne elipsoidalne fi, lambda, h
     """
-    
-    def XYZ2FLH(self, X, Y, Z):
+    def XYZ2FLH(self, X,Y,Z):
         transformed = []
-        
-        """Zastosowano algorytm Hirvonena, transformujący współrzędne prostokątne na współrzędne elipsoidalne. W procesie iteracyjnym, uzyskujemy dokładne wyniki"""
-        for X, Y, Z in zip(X, Y, Z):
-            r   = np.sqrt(X**2 + Y**2)           # promień
-            Fi = atan(Z / (r * (1 - self.ep2)))    # pierwsze przybliilizenie
-            fi = 0
-            while abs(Fi - fi) > 0.000001/206265:    
-                Fi = fi
-                N = self.Np(Fi)
-                h = r / cos(Fi) - N
-                lat = atan((Z/r) * (((1 - self.ep2 * N/(N + h))**(-1))))
-                break
-            lam = np.arctan(Y/X)
-            N = self.a / sqrt(1 - self.ep2 * (sin(Fi))**2);
-            h = r / cos(Fi) - N  
-            transformed.append([np.rad2deg(Fi), np.rad2deg(lam), h])
-        return transformed
-    
-
-        """
-        Transformacja współrzędnych elipsoidalnych fi, lambda, h na współrzędne XYZ
-        """
-        def FLH2XYZ(self,Fi,lam,h):
-            transformed = []
+        for X, Y, Z in zip(X,Y,Z):
+            p = np.sqrt(X**2+Y**2)
+            f = np.arctan(Z/(p*(1-self.ep2)))
             while True:
+                N =self.Np(f)
+                h=(p/np.cos(f))-N
+                fp=f
+                f=np.arctan(Z/(p*(1-self.ep2*N/(N+h))))
+                if abs(fp-f)<(0.000001/206265):
+                    break
+            l=np.arctan2(Y,X)
+            transformed.append([np.rad2deg(f), np.rad2deg(l), h])
+        return transformed   
+    """
+   Transformacja współrzędnych elipsoidalnych fi, lambda, h na współrzędne XYZ"""
+    def FLH2XYZ(self,Fi,lam,h):
+        transformed = []
+        for Fi, lam, h in zip(Fi, lam, h):
+            while True:
+                Fi = np.deg2rad(Fi)
+                lam = np.deg2rad(lam)
                 N=self.Np(Fi)
                 Xk=(N+h)*np.cos(Fi)*np.cos(lam)
                 Yk=(N+h)*np.cos(Fi)*np.sin(lam)
@@ -62,86 +56,83 @@ class Transformacje:
                 if abs(Xp-Xk)<(0.000001/206265):
                     break
                 transformed.append([Xk, Yk, Zk])
-            return transformed
-        
-        """
-        Tranformacja współrzędnych geocentryczny do współrzędnych topocentrycznych
-        """
-        def Rneu(f,l):
-            R = np.array([[-np.sin(f)*np.cos(l), -np.sin(l), np.cos(f)*np.cos(l)],
-                          [-np.sin(f)*np.sin(l), np.cos(l), np.cos(f)*np.sin(l)],
-                          [np.cos(f), 0., np.sin(f)]])
-            return(R)
-        
+        return transformed
+    """Tranformacja współrzędnych geocentryczny do współrzędnych topocentrycznych"""
+    def Rneu(f,l):
+        R = np.array([[-np.sin(f)*np.cos(l), -np.sin(l), np.cos(f)*np.cos(l)],
+                      [-np.sin(f)*np.sin(l), np.cos(l), np.cos(f)*np.sin(l)],
+                      [np.cos(f), 0., np.sin(f)]])
+        return(R)
     
-        def XYZ2NEU(self, X, Y, Z, X0, Y0, Z0):
-            transformed = []
-            r   = np.sqrt(X**2 + Y**2)           # promień
-            Fi = atan(Z / (r * (1 - self.ep2)))    # pierwsze przybliilizenie
-            fi = 0
-            while abs(Fi - fi) > 0.000001/206265:    
-                Fi = fi
-                N = self.Np(f)
-                h = r / np.cos(Fi) - N
-                lat = atan((Z/r) * (((1 - self.ep2 * N/(N + h))**(-1))))
-                break
-            lam = np.arctan(Y/X)
-            N = self.a / sqrt(1 - self.ep2 * (sin(fi))**2);
-            h = r / cos(fi) - N  
-            
-            R_neu = self.Rneu(f,l)
-            for X, Y, Z in zip(X, Y, Z):
-                X_sr = [X-X0, Y-Y0, Z-Z0] 
-                X_rneu = R_neu.T@X_sr
-                transformed.append(X_rneu.T)
-                
-            return transformed
 
-        """
-        Tranformacja współrzędnych fi, lambda do układu 2000
-        """
-        
-        def sigma(self, f):
-            A0 = 1 - self.ep2/4 - 3 * self.ep2**2/64 - 5 * self.ep2**3/256
-            A2 = (3/8) * (self.ep2 + self.ep2**2/4 + 15 * self.ep2**3/128)
-            A4 = (15/256) * (self.ep2**2 + 3 * self.ep2**3/4)
-            A6 = 35 * self.ep2**3/3072
-            sig = self.a * (A0 * f - A2 * np.sin(2 * f) + A4 * np.sin(4 * f) - A6 * np.sin(6 * f))
-            return(sig)
-        
-        def GK2000(self, f, l, m=0.999923):
-            transformed = []
-            for f, l in zip(f,l):
-                l0 = 0 
-                strefa = 0
-                if l >np.deg2rad(13.5) and l < np.deg2rad(16.5):
-                    strefa = 5
-                    la0 = np.deg2rad(15)
-                elif l >np.deg2rad(16.5) and l < np.deg2rad(19.5):
-                    strefa = 6
-                    l0 = np.deg2rad(18)
-                elif l >np.deg2rad(19.5) and l < np.deg2rad(22.5):
-                    strefa =7
-                    l0 = np.deg2rad(21)
-                elif l >np.deg2rad(22.5) and l < np.deg2rad(25.5):
-                    strefa = 8
-                    l0 = np.deg2rad(24)
-                else:
-                    return("Punkt poza strefami odwzorowawczymi układu PL-2000")        
-        
-            b2 = (slef.a**2) * (1-self.ep2)   #krotsza polos
-            e2p = ( self.a**2 - b2 ) / b2   #drugi mimosrod elipsy
-            dl = l - l0
-            t = np.tan(f)
-            ni = np.sqrt(e2p * (np.cos(f))**2)
+    def XYZ2NEU(self, X, Y, Z, X0, Y0, Z0):
+        transformed = []
+        r   = np.sqrt(X**2 + Y**2)           # promień
+        Fi = atan(Z / (r * (1 - self.ep2)))    # pierwsze przybliilizenie
+        fi = 0
+        while abs(Fi - fi) > 0.000001/206265:    
+            Fi = fi
             N = self.Np(f)
-            sigma = self.sigma(f)
-            XGK20 = sigma + ((dl**2)/2)*N*np.sin(f)*np.cos(f) * ( 1 + ((dl**2)/12)*(np.cos(f))**2 * ( 5 - (t**2)+9*(ni**2) + 4*(ni**4)     )  + ((dl**4)/360)*(np.cos(f)**4) * (61-58*(t**2)+(t**4) + 270*(ni**2) - 330*(ni**2)*(t**2))  )
-            YGK20 = (dl*N* np.cos(f)) * (1+(((dl)**2/6)*(np.cos(f))**2) *(1-(t**2)+(ni**2))+((dl**4)/120)*(np.cos(f)**4)*(5-18*(t**2)+(t**4)+14*(ni**2)-58*(ni**2)*(t**2)) )
-            X2000 = XGK20 * m 
-            Y2000 = YGK20 * m + strefa*1000000 + 500000
-            transformed.append([X2000, Y2000])
+            h = r / np.cos(Fi) - N
+            lat = atan((Z/r) * (((1 - self.ep2 * N/(N + h))**(-1))))
+            break
+        lam = np.arctan(Y/X)
+        N = self.a / sqrt(1 - self.ep2 * (sin(fi))**2);
+        h = r / cos(fi) - N  
+        
+        R_neu = self.Rneu(f,l)
+        for X, Y, Z in zip(X, Y, Z):
+            X_sr = [X-X0, Y-Y0, Z-Z0] 
+            X_rneu = R_neu.T@X_sr
+            transformed.append(X_rneu.T)
             
+        return transformed
+
+    """
+    Tranformacja współrzędnych fi, lambda do układu 2000
+    """
+    
+    def sigma(self, f):
+        A0 = 1 - self.ep2/4 - 3 * self.ep2**2/64 - 5 * self.ep2**3/256
+        A2 = (3/8) * (self.ep2 + self.ep2**2/4 + 15 * self.ep2**3/128)
+        A4 = (15/256) * (self.ep2**2 + 3 * self.ep2**3/4)
+        A6 = 35 * self.ep2**3/3072
+        sig = self.a * (A0 * f - A2 * np.sin(2 * f) + A4 * np.sin(4 * f) - A6 * np.sin(6 * f))
+        return(sig)
+    
+    def GK2000(self, f, l, m=0.999923):
+        transformed = []
+        for f, l in zip(f,l):
+            l0 = 0 
+            strefa = 0
+            if l >np.deg2rad(13.5) and l < np.deg2rad(16.5):
+                strefa = 5
+                la0 = np.deg2rad(15)
+            elif l >np.deg2rad(16.5) and l < np.deg2rad(19.5):
+                strefa = 6
+                l0 = np.deg2rad(18)
+            elif l >np.deg2rad(19.5) and l < np.deg2rad(22.5):
+                strefa =7
+                l0 = np.deg2rad(21)
+            elif l >np.deg2rad(22.5) and l < np.deg2rad(25.5):
+                strefa = 8
+                l0 = np.deg2rad(24)
+            else:
+                return("Punkt poza strefami odwzorowawczymi układu PL-2000")        
+    
+        b2 = (self.a**2) * (1-self.ep2)   #krotsza polos
+        e2p = ( self.a**2 - b2 ) / b2   #drugi mimosrod elipsy
+        dl = l - l0
+        t = np.tan(f)
+        ni = np.sqrt(e2p * (np.cos(f))**2)
+        N = self.Np(f)
+        sigma = self.sigma(f)
+        XGK20 = sigma + ((dl**2)/2)*N*np.sin(f)*np.cos(f) * ( 1 + ((dl**2)/12)*(np.cos(f))**2 * ( 5 - (t**2)+9*(ni**2) + 4*(ni**4)     )  + ((dl**4)/360)*(np.cos(f)**4) * (61-58*(t**2)+(t**4) + 270*(ni**2) - 330*(ni**2)*(t**2))  )
+        YGK20 = (dl*N* np.cos(f)) * (1+(((dl)**2/6)*(np.cos(f))**2) *(1-(t**2)+(ni**2))+((dl**4)/120)*(np.cos(f)**4)*(5-18*(t**2)+(t**4)+14*(ni**2)-58*(ni**2)*(t**2)) )
+        X2000 = XGK20 * m 
+        Y2000 = YGK20 * m + strefa*1000000 + 500000
+        transformed.append([X2000, Y2000])
+        
         return transformed
     
     """
@@ -176,20 +167,20 @@ class Transformacje:
         print('tu zapisuje')
         if transf == 'XYZ2FLH':
             transformed  = self.XYZ2FLH(dane[:,0], dane[:,1], dane[:,2])
-            np.savetxt(f"dane_{transf}_{args.elip}.txt", transformed, delimiter=' ', fmt='%0.10f %0.10f %0.3f')
+            np.savetxt(f"wynik_{transf}_{args.elip}.txt", transformed, delimiter=' ', fmt='%0.10f %0.10f %0.3f')
         elif transf == 'FLH2XYZ':
             print('jestes tutaj')
             transformed  = self.FLH2XYZ(np.deg2rad((dane[:,0])), np.deg2rad(dane[:,1]), dane[:,2])
-            np.savetxt(f"plik_wynikowy_{transf}_{args.elip}.txt", transformed, delimiter =' ', fmt ='%0.3f %0.3f %0.3f' )
+            np.savetxt(f"wynik_{transf}_{args.elip}.txt", transformed, delimiter =' ', fmt ='%0.3f %0.3f %0.3f' )
         elif transf == 'XYZ2NEU':
             transformed  = self.XYZ2NEU(dane[1:,0], dane[1:,1], dane[1:,2], dane[0,0], dane[0,1], dane[0,2])
-            np.savetxt(f"plik_wynikowy_{transf}_{args.elip}.txt", transformed, delimiter =' ', fmt ='%0.3f %0.3f %0.3f' )
+            np.savetxt(f"wynik_{transf}_{args.elip}.txt", transformed, delimiter =' ', fmt ='%0.3f %0.3f %0.3f' )
         elif transf == 'GK2000':
             transformed  = self.GK2000(np.deg2rad(dane[:,0]), np.deg2rad(dane[:,1]))
-            np.savetxt(f"plik_wynikowy_{transf}_{args.elip}.txt", transformed, delimiter=' ', fmt='%0.3f %0.3f')
+            np.savetxt(f"wynik_{transf}_{args.elip}.txt", transformed, delimiter=' ', fmt='%0.3f %0.3f')
         elif transf == 'GK1992':
             transformed  = self.GK1992(np.deg2rad(dane[:,0]), np.deg2rad(dane[:,1]))
-            np.savetxt(f"plik_wynikowy_{transf}_{args.elip}.txt", transformed, delimiter=' ', fmt='%0.3f %0.3f')
+            np.savetxt(f"wynik_{transf}_{args.elip}.txt", transformed, delimiter=' ', fmt='%0.3f %0.3f')
 
 if __name__ == '__main__':
     parser = ArgumentParser()
