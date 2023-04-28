27b4e853edd7dda0fdfffa942da8fc7665d9827e
Malgorzata Barej <barejmalgosia@gmail,com>
2023-04-28 23:29:12 +0200

    wszystko działa:)))

--- skrypt.py
+++ skrypt.py
@@ -26,7 +26,7 @@ class Transformacje:
     Tranformacja współrzędnych geocentrycznych XYZ na współrzędne elipsoidalne fi, lambda, h
     """
     def XYZ2FLH(self, X,Y,Z):
-        transformed = []
+        result = []
         for X, Y, Z in zip(X,Y,Z):
             p = np.sqrt(X**2+Y**2)
             f = np.arctan(Z/(p*(1-self.ep2)))
@@ -38,12 +38,14 @@ class Transformacje:
                 if abs(fp-f)<(0.000001/206265):
                     break
             l=np.arctan2(Y,X)
-            transformed.append([np.rad2deg(f), np.rad2deg(l), h])
-        return transformed   
+            N = self.Np(f)
+            h = (p/np.cos(f))-N
+            result.append([np.rad2deg(f), np.rad2deg(l), h])
+        return result  
     """
    Transformacja współrzędnych elipsoidalnych fi, lambda, h na współrzędne XYZ"""
     def FLH2XYZ(self,Fi,lam,h):
-        transformed = []
+        result = []
         for Fi, lam, h in zip(Fi, lam, h):
             while True:
                 Fi = np.deg2rad(Fi)
@@ -55,8 +57,9 @@ class Transformacje:
                 Xp = 0
                 if abs(Xp-Xk)<(0.000001/206265):
                     break
-                transformed.append([Xk, Yk, Zk])
-        return transformed
+            result.append([Xk, Yk, Zk])
+        return result
+    
     """Tranformacja współrzędnych geocentryczny do współrzędnych topocentrycznych"""
     def Rneu(self,f,l):
         R = np.array([[-np.sin(f)*np.cos(l), -np.sin(l), np.cos(f)*np.cos(l)],
@@ -66,27 +69,27 @@ class Transformacje:
     
 
     def XYZ2NEU(self, X, Y, Z, X0, Y0, Z0):
-        transformed = []
-        r   = np.sqrt(X**2 + Y**2)           # promień
-        Fi = atan(Z / (r * (1 - self.ep2)))    # pierwsze przybliilizenie
-        fi = 0
-        while abs(Fi - fi) > 0.000001/206265:    
-            Fi = fi
-            N = self.Np(f)
-            h = r / np.cos(Fi) - N
-            lat = atan((Z/r) * (((1 - self.ep2 * N/(N + h))**(-1))))
-            break
-        lam = np.arctan(Y/X)
-        N = self.a / sqrt(1 - self.ep2 * (sin(fi))**2);
-        h = r / cos(fi) - N  
+        result = []
+        p = np.sqrt(X0**2+Y0**2)
+        f = np.arctan(Z0/(p*(1-self.ep2)))
+        while True:
+            N =self.Np(f)
+            h=(p/np.cos(f))-N
+            fp=f
+            f=np.arctan(Z0/(p*(1-self.ep2*N/(N+h))))
+            if abs(fp-f)<(0.000001/206265):
+                break
+        l=np.arctan2(Y0,X0)
+        N = self.Np(f)
+        h = p / cos(f) - N
         
         R_neu = self.Rneu(f,l)
         for X, Y, Z in zip(X, Y, Z):
             X_sr = [X-X0, Y-Y0, Z-Z0] 
             X_rneu = R_neu.T@X_sr
-            transformed.append(X_rneu.T)
+            result.append(X_rneu.T)
             
-        return transformed
+        return result
 
     """
     Tranformacja współrzędnych fi, lambda do układu 2000
@@ -101,7 +104,7 @@ class Transformacje:
         return(sig)
     
     def GK2000(self, f, l, m=0.999923):
-        transformed = []
+        result = []
         for f, l in zip(f,l):
             l0 = 0 
             strefa = 0
@@ -131,16 +134,16 @@ class Transformacje:
         YGK20 = (dl*N* np.cos(f)) * (1+(((dl)**2/6)*(np.cos(f))**2) *(1-(t**2)+(ni**2))+((dl**4)/120)*(np.cos(f)**4)*(5-18*(t**2)+(t**4)+14*(ni**2)-58*(ni**2)*(t**2)) )
         X2000 = XGK20 * m 
         Y2000 = YGK20 * m + strefa*1000000 + 500000
-        transformed.append([X2000, Y2000])
+        result.append([X2000, Y2000])
         
-        return transformed
+        return result
     
     """
     Tranformacja współrzędnych fi, lambda do układu 1992
     """
     
     def GK1992(self, f, l, m = 0.9993):
-        transformed = []
+        result = []
         lam0 = (19*np.pi)/180
         for f, l in zip(f,l):
             b2 = (self.a**2) * (1-self.ep2)   #krotsza polos
@@ -158,48 +161,41 @@ class Transformacje:
             x92 = xgk*m - 5300000
             y92 = ygk*m + 500000
             
-            transformed.append([x92, y92])
+            result.append([x92, y92])
         
-        return transformed 
+        return result 
 
-    def plik(self, pliczek, transf):
-        dane = np.genfromtxt(pliczek,delimiter = ' ')
-        print('tu zapisuje')
+    def plik(self, file, transf):
+        dane = np.genfromtxt(file,delimiter = ' ')
         if transf == 'XYZ2FLH':
-            transformed  = self.XYZ2FLH(dane[:,0], dane[:,1], dane[:,2])
-            np.savetxt(f"wynik_{transf}_{args.elip}.txt", transformed, delimiter=' ', fmt='%0.10f %0.10f %0.3f')
+            result  = self.XYZ2FLH(dane[:,0], dane[:,1], dane[:,2])
+            np.savetxt(f"C:/Users/USER/OneDrive/Pulpit/p1/result_{transf}_{args.elip}.txt", result, delimiter=' ', fmt='%0.10f %0.10f %0.3f')
         elif transf == 'FLH2XYZ':
-            print('jestes tutaj')
-            transformed  = self.FLH2XYZ(np.deg2rad((dane[:,0])), np.deg2rad(dane[:,1]), dane[:,2])
-            np.savetxt(f"wynik_{transf}_{args.elip}.txt", transformed, delimiter =' ', fmt ='%0.3f %0.3f %0.3f' )
+            result  = self.FLH2XYZ(np.deg2rad((dane[:,0])), np.deg2rad(dane[:,1]), dane[:,2])
+            np.savetxt(f"C:/Users/USER/OneDrive/Pulpit/p1/result_{transf}_{args.elip}.txt", result, delimiter =' ', fmt ='%0.3f %0.3f %0.3f' )
         elif transf == 'XYZ2NEU':
-            print('tutaj')
-            transformed  = self.XYZ2NEU(dane[1:,0], dane[1:,1], dane[1:,2], dane[0,0], dane[0,1], dane[0,2])
-            np.savetxt(f"wynik_{transf}_{args.elip}.txt", transformed, delimiter =' ', fmt ='%0.3f %0.3f %0.3f' )
+            result  = self.XYZ2NEU(dane[1:,0], dane[1:,1], dane[1:,2], dane[0,0], dane[0,1], dane[0,2])
+            np.savetxt(f"C:/Users/USER/OneDrive/Pulpit/p1/result_{transf}_{args.elip}.txt", result, delimiter =' ', fmt ='%0.3f %0.3f %0.3f' )
         elif transf == 'GK2000':
-            transformed  = self.GK2000(np.deg2rad(dane[:,0]), np.deg2rad(dane[:,1]))
-            np.savetxt(f"wynik_{transf}_{args.elip}.txt", transformed, delimiter=' ', fmt='%0.3f %0.3f')
+            result  = self.GK2000(np.deg2rad(dane[:,0]), np.deg2rad(dane[:,1]))
+            np.savetxt(f"C:/Users/USER/OneDrive/Pulpit/p1/result_{transf}_{args.elip}.txt", result, delimiter=' ', fmt='%0.3f %0.3f')
         elif transf == 'GK1992':
-            transformed  = self.GK1992(np.deg2rad(dane[:,0]), np.deg2rad(dane[:,1]))
-            np.savetxt(f"wynik_{transf}_{args.elip}.txt", transformed, delimiter=' ', fmt='%0.3f %0.3f')
+            result  = self.GK1992(np.deg2rad(dane[:,0]), np.deg2rad(dane[:,1]))
+            np.savetxt(f"C:/Users/USER/OneDrive/Pulpit/p1/result_{transf}_{args.elip}.txt", result, delimiter=' ', fmt='%0.3f %0.3f')
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-pliczek', type=str, help='Wpisz sciezke do pliku z danymi wejsciowymi')
+    parser.add_argument('-dane', type=str, help='Wpisz sciezke do pliku z danymi wejsciowymi')
     parser.add_argument('-elip', type=str, help='Wybierz elipsoide sposrod dostepnych: WRS84, GRS80, KRASOWSKI')
     parser.add_argument('-transf', type=str, help='Wybierz transformacje, z ktorej chcesz skorzystac, sposrod dostepnych: XYZ2flh, flh2XYZ, saz2neu, GK2000, GK1992, XYZ2NEU')
     args = parser.parse_args()
     elip = {'WGS84': [6378137.000, 0.00669438002290], 'GRS80': [6378137.000, 0.00669438002290], 'KRASOWSKI': [6378245.000, 0.00669342162296]}
     transf = {'XYZ2FLH': 'XYZ2FLH', 'FLH2XYZ': 'FLH2XYZ','XYZ2NEU': 'XYZ2NEU', 'GK2000': 'GK2000', 'GK1992': 'GK1992'}
-    # print(args.elip, elip[args.elip])
-    # print(elip['WGS84'][1])
-    # print(args.pliczek)
-    # wsp = Transformacje(elip[args.elip.upper()])
-    # wczyt = wsp.plik(args.pliczek, transf[args.transf.upper()])
+
     
     try:
         wsp = Transformacje(elip[args.elip])
-        wczyt = wsp.plik(args.pliczek, transf[args.transf.upper()])
+        wczyt = wsp.plik(args.dane, transf[args.transf.upper()])
         print("Utworzono plik ze wspolrzednymi.")
     except AttributeError as e:
         print("Error:", e)    
